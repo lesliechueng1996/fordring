8af00b5ba4145b0dfbd72882cbd2b4b7d6ada6ca
LeslieCheung <lesliechueng1996@icloud.com>
2023-10-07 20:55:33 +0800

    add validation lib

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './filters/global-exception.filter';
 import WinstonLogger from './configs/logger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,9 +10,13 @@ async function bootstrap() {
     cors: true,
   });
 
+  // global exception filter
   const { httpAdapter }: { httpAdapter: HttpAdapterHost } =
     app.get(HttpAdapterHost);
   app.useGlobalFilters(new GlobalExceptionFilter(httpAdapter));
+
+  // global validation pipe
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
