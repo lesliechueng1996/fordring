3986639e3fa5d0c9e1c9a732faeb58a815b02fc7
LeslieCheung <lesliechueng1996@icloud.com>
2023-10-11 02:03:29 +0800

    layout change

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -8,8 +8,10 @@ function MainPage() {
       <Header />
       <div className="grow flex">
         <AsideMenu />
-        <main className="p-5">
-          <Outlet />
+        <main className="p-5 grow">
+          <div className="shadow h-full border rounded-md p-10">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
