94b99b6663daefc80cbfa864c35a6165c18d6fd2
LeslieCheung <lesliechueng1996@icloud.com>
2023-10-21 23:23:18 +0800

    save draft article, and picture mapping

--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -1,14 +1,30 @@
-import { Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ArticleService } from './article.service';
 import {
   ApiBadRequestResponse,
+  ApiCreatedResponse,
   ApiExtraModels,
   ApiHeader,
+  ApiOperation,
   ApiTags,
 } from '@nestjs/swagger';
 import { AuthGuard } from 'src/guards/AuthGuard';
-import { AUTHENTICATION } from 'src/constants/fordring.const';
-import { ApiJsonResult } from 'src/dto/api-json-result.dto';
+import { AUTHENTICATION, USER_ID_KEY } from 'src/constants/fordring.const';
+import {
+  ApiJsonResult,
+  ApiJsonResultResponse,
+} from 'src/dto/api-json-result.dto';
+import { SaveDraftDtoReq, SaveDraftDtoRes } from './dto/save-draft.dto';
+import { Request } from 'express';
+import { UserService } from '../user/user.service';
 
 @Controller('article')
 @ApiTags('Article')
@@ -17,10 +33,31 @@ import { ApiJsonResult } from 'src/dto/api-json-result.dto';
 @ApiExtraModels(ApiJsonResult)
 @ApiBadRequestResponse({ description: '参数错误' })
 export class ArticleController {
-  constructor(private readonly articleService: ArticleService) {}
+  constructor(
+    private readonly articleService: ArticleService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('/draft')
-  async saveDraft() {}
+  @ApiOperation({ summary: '保存草稿' })
+  @ApiCreatedResponse({ description: '保存成功' })
+  @ApiJsonResultResponse(SaveDraftDtoRes)
+  async saveDraft(
+    @Body() body: SaveDraftDtoReq,
+    @Req() req: Request,
+  ): Promise<SaveDraftDtoRes> {
+    const userId: string = req[USER_ID_KEY];
+    const { title, content } = body;
+    const user = await this.userService.getUserById(userId);
+    const article = await this.articleService.saveDraftArticle(
+      title,
+      content,
+      user,
+    );
+    return {
+      id: article.id,
+    };
+  }
 
   @Patch('/draft/:id')
   async updateDraft(@Param('id') id: string) {
