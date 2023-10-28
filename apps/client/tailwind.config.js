d7c15a8b330591e4061ad3eab393a32a8de7ca97
LeslieCheung <lesliechueng1996@icloud.com>
2023-10-28 12:47:10 +0800

    add global css and image

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -9,8 +9,17 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        label: 'rgb(var(--label-color))',
+        'label-colorful': 'rgb(var(--label-colorful-color))',
+        'label-colorful-sub': 'rgb(var(--label-colorful-color-sub))',
+        'content-main': 'rgb(var(--content-main-color))',
+        'content-sub': 'rgb(var(--content-sub-color))',
+        background: 'rgb(var(--background-color))',
+        'deep-background': 'rgb(var(--deep-background-color))',
+      },
+    },
   },
   plugins: [],
-}
-
+};
